886cafbf27fe5bde99d1a14da6fa283558c069c4
Acbox liu <850625057@qq.com>
2024-10-05 14:30:21 +0800

    feat: add mobile adaption

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,32 @@
 <template>
-  <div class="w-full h-[5000px]" style="background: linear-gradient(to bottom, white, lightyellow, orange);">
-    <div class="h-screen">
-      <div class="flex flex-col px-64 mt-5 w-full">
-        <div class="float-left">
-          <img src="./assets/dromara.png" class="w-40 flex" />
-        </div>
-        <div class="flex flex-row w-full">
-          <div class="flex flex-col mt-32">
-            <transition appear name="slide-fade">
-              <div v-show="show" class="text-[150px] text-amber-700 font-bold animate-item" style="--delay: 0s;">CosCon
-                2024</div>
-            </transition>
-            <transition appear name="slide-fade">
-              <div v-show="show" class="text-4xl text-amber-700 font-bold pl-5 animate-item" style="--delay: 0.5s;">
-                Dromara分会场</div>
-            </transition>
-          </div>
-          <div class="flex justify-center items-center pt-[300px] pl-[300px]">
-            <div class="w-[300px] h-[300px]">
-              <img src="./assets/qrcode.png" class="w-full h-full object-cover" alt="QR Code" />
-            </div>
-          </div>
-        </div>
-        <hr class="border-white mt-20" />
+  <div class="w-full min-h-screen" style="background: linear-gradient(to bottom, white, lightyellow, orange);">
+    <div class="flex flex-col px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64 w-full h-screen">
+      <div class="float-left mt-4">
+        <img src="./assets/dromara.png" class="w-20 sm:w-32 md:w-40 flex" />
+      </div>
+      <div class="flex flex-col lg:flex-row w-full mt-8 lg:mt-16">
         <div class="flex flex-col">
           <transition appear name="slide-fade">
-            <div v-show="show" class="text-5xl text-amber-700 font-bold pl-5 animate-item" style="--delay: 1s;">Beijing
-              / 北京</div>
+            <div v-show="show" class="text-4xl sm:text-6xl md:text-7xl lg:text-[80px] text-amber-700 font-bold animate-item" style="--delay: 0s;">CosCon 2024</div>
           </transition>
           <transition appear name="slide-fade">
-            <div v-show="show" class="text-3xl text-amber-700 font-bold pl-5 animate-item" style="--delay: 1.5s;">
-              2024.11.2</div>
+            <div v-show="show" class="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-amber-700 font-bold animate-item" style="--delay: 0.5s;">Dromara分会场</div>
           </transition>
         </div>
+        <div class="flex justify-center items-center mt-8 lg:mt-0 lg:ml-auto">
+          <div class="w-40 h-40 sm:w-60 sm:h-60 md:w-80 md:h-80 lg:w-[300px] lg:h-[300px]">
+            <img src="./assets/qrcode.png" class="w-full h-full object-cover" alt="QR Code" />
+          </div>
+        </div>
+      </div>
+      <hr class="border-white mt-8 lg:mt-20" />
+      <div class="flex flex-col mt-4 lg:mt-8">
+        <transition appear name="slide-fade">
+          <div v-show="show" class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-amber-700 font-bold animate-item" style="--delay: 1s;">Beijing / 北京</div>
+        </transition>
+        <transition appear name="slide-fade">
+          <div v-show="show" class="text-xl sm:text-2xl md:text-2xl lg:text-3xl text-amber-700 font-bold mt-2 animate-item" style="--delay: 1.5s;">2024.10.2</div>
+        </transition>
       </div>
       <div class="flex items-center justify-center">
         <div @click="scrollToView()"
@@ -40,66 +34,56 @@
           开启变革</div>
       </div>
     </div>
-
-    <div class="text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">大会介绍</div>
-    <div class="text-xl text-amber-600 font-bold items-center flex justify-center mt-20 px-[300px]" id="des">
-      中国开源年会（COSCon）是业界最具影响力的开源盛会之一，自2015年由开源社首次发起以来，今年即将迎来第九届。凭借其独特定位及不断增长的影响力，COSCon
-      不仅获得了越来越多国内外企业、高校以及开源组织和社区的鼎力支持，而且与一般由企业、IT
-      媒体或行业协会举办的行业大会相比，它拥有更广泛的跨组织、跨项目、跨社区的覆盖范围，因此成功吸引了大量来自全球的开源开发者、贡献者和提交者的高度关注与积极参与。
-    </div>
-
-    <div class="text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">大会地址</div>
-    <div class="text-[30px] text-amber-600 font-bold items-center flex justify-center" id="des">北京中关村国家自主创新示范区会议中心
-    </div>
-    <div class="text-xl text-amber-600 font-bold items-center flex justify-center mt-20 px-[300px]" id="des">
-      <iframe
-        src="https://m.amap.com/navi/?dest=116.293344,39.989479&destName=北京中关村国家自主创新示范区会议中心&hideRouteIcon=1&key=584d85ce77e3a854180cd921c9a41977&jscode=bb0d66965cd65d1f4678312df26b9e37"
-        class="w-full h-[1000px]" />
-    </div>
-
-    <div class="text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">演讲成员</div>
-    <div class="text-2xl text-amber-600 font-bold items-center flex justify-center mt-20 px-[300px]" id="des">
-      <div class="w-full">
-        <table class="w-full border-amber-600 border-2">
-          <tr class="w-full border-amber-600 border-2">
-            <td>猫大人（负责人）</td>
-            <td>唯一解（演讲嘉宾）</td>
-            <td>阿超（演讲嘉宾）</td>
-            <td>风如歌（演讲嘉宾）</td>
-            <td>Acbox（演讲嘉宾）</td>
-          </tr>
-          <tr class="w-full border-amber-600 border-2">
-            <td>NicholasLD</td>
-            <td>萧帽</td>
-            <td>付守江</td>
-            <td>疯狂的狮子Li</td>
-            <td>读钓</td>
-            <td>mouday</td>
-          </tr>
-          <tr class="border-amber-600 border-2">
-            <td>失败女神</td>
-            <td>唐振超（方糖先生）</td>
-            <td>yanhom</td><td>sh1yu</td>
-            <td>小眼</td>
-          </tr>
-        </table>
+    
+    <!-- 其他部分的移动端适配 -->
+    <div class="px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64">
+      <div class="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">大会介绍</div>
+      <div class="text-base sm:text-lg md:text-xl text-amber-600 font-bold items-center flex justify-center mt-10 sm:mt-20">
+        中国开源年会（COSCon）是业界最具影响力的开源盛会之一，自2015年由开源社首次发起以来，今年即将迎来第九届。凭借其独特定位及不断增长的影响力，COSCon
+        不仅获得了越来越多国内外企业、高校以及开源组织和社区的鼎力支持，而且与一般由企业、IT
+        媒体或行业协会举办的行业大会相比，它拥有更广泛的跨组织、跨项目、跨社区的覆盖范围，因此成功吸引了大量来自全球的开源开发者、贡献者和提交者的高度关注与积极参与。
       </div>
+
+      <div class="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">大会地址</div>
+      <div class="text-xl sm:text-2xl md:text-[30px] text-amber-600 font-bold items-center flex justify-center text-center mt-5">北京中关村国家自主创新示范区会议中心</div>
+      <div class="mt-10 sm:mt-20">
+        <iframe
+          src="https://m.amap.com/navi/?dest=116.293344,39.989479&destName=北京中关村国家自主创新示范区会议中心&hideRouteIcon=1&key=584d85ce77e3a854180cd921c9a41977&jscode=bb0d66965cd65d1f4678312df26b9e37"
+          class="w-full h-[300px] sm:h-[500px] md:h-[700px] lg:h-[1000px]" />
+      </div>
+
+      <div class="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">演讲成员</div>
+      <div class="text-base sm:text-lg md:text-xl lg:text-2xl text-amber-600 font-bold items-center flex justify-center mt-10 sm:mt-20">
+        <div class="w-full overflow-x-auto">
+          <table class="w-full border-amber-600 border-2">
+            <tr class="w-full border-amber-600 border-2">
+              <td>猫大人（负责人）</td>
+              <td>唯一解（演讲嘉宾）</td>
+              <td>阿超（演讲嘉宾）</td>
+              <td>风如歌（演讲嘉宾）</td>
+              <td>Acbox（演讲嘉宾）</td>
+            </tr>
+            <tr class="w-full border-amber-600 border-2">
+              <td>NicholasLD</td>
+              <td>萧帽</td>
+              <td>付守江</td>
+              <td>疯狂的狮子Li</td>
+              <td>读钓</td>
+              <td>mouday</td>
+            </tr>
+            <tr class="border-amber-600 border-2">
+              <td>失败女神</td>
+              <td>唐振超（方糖先生）</td>
+              <td>yanhom</td><td>sh1yu</td>
+              <td>小眼</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+
+      <div class="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">时间线</div>
+      <div class="text-xl sm:text-2xl md:text-[30px] text-amber-600 font-bold items-center flex justify-center">待定</div>
     </div>
-
-    <div class="text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">时间线</div>
-    <div class="text-[30px] text-amber-600 font-bold items-center flex justify-center" id="des">待定</div>
-
-    <!-- <div class="text-[80px] text-amber-600 font-bold items-center flex justify-center mt-20" id="des">组织成员项目</div>
-    <div class="mt-20 flex flex-row p-10">
-      <img :src="url" v-for="url in [
-        './assets/Akali.webp',
-        './assets/logos/CloudEon.webp',
-        './assets/logos/cubic.webp',
-        './assets/logos/dante-cloud.webp',
-        './assets/logos/dax-pay.webp',
-        './assets/logos/dbswitch.webp'
-      ]" class="w-[200px] p-3"/>
-    </div> -->
   </div>
 </template>
 
